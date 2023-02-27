ecbad57bcd3ff97361bb50d4eba8baa3abe6d066
Niklas Bakula <niklasjbakula@gmail.com>
2023-02-27 15:05:41 +0000

    Added seeds file but does not work yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+email_suffix = %w[gmail.com hotmail.de live.com yahoo.com outlook.com]
+car_brand = %w[Volvo BMW VW Audi Tesla Toyota]
+car_type = %w[SUV sedan sportscar limousine]
+
+10.times do
+  Car.new()
+
+end
+
+
+
+10.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  email = "#{first_name}.#{last_name}@#{email_suffix.sample}"
+  User.create(first_name: first_name,
+              last_name: last_name,
+              email: email)
+
+end
