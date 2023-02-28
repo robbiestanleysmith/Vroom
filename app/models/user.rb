503c75a9851cfa7ab7a76c32644eb83a83679412
Felix Mitchell <felix.mitchell@kcl.ac.uk>
2023-02-28 15:22:38 +0000

    photo changed to photos in seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
-  validates :avatar, presence: true
-  validates :host_rating, presence: true
-  validates :host_answer, presence: true
-  validates :host_answer, inclusion: [true, false]
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :email, presence: true
+  # validates :email, uniqueness: true
+  # validates :avatar, presence: true
+  # validates :host_rating, presence: true
+  # validates :host_answer, presence: true
+  # validates :host_answer, inclusion: [true, false]
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
 end
