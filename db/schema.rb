57f654412c6e81336b791cd24b4b93fecefe95dc
Neil Cairns <neilcairns@me.com>
2023-02-28 17:20:27 +0000

    db migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_141050) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_171357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_141050) do
     t.string "mileage"
     t.float "price_per_day"
     t.string "location"
+    t.text "description"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
