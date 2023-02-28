844e99781d90a5dd040f83d4a7969df95bfc548b
Felix Mitchell <felix.mitchell@kcl.ac.uk>
2023-02-28 15:19:11 +0000

    jules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
@@ -8,10 +7,4 @@ class User < ApplicationRecord
   validates :host_rating, presence: true
   validates :host_answer, presence: true
   validates :host_answer, inclusion: [true, false]
-=======
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
->>>>>>> eabf56a8c92b84e4fa39747b261d2f27b604c5c9
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_114238) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_02_28_141050) do
->>>>>>> 51d55fcfd5e30fc46664279df6f2e7c04c6d39a7
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,14 +77,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_141050) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
     t.text "avatar"
     t.string "rating"
     t.boolean "host_answer"
-=======
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> eabf56a8c92b84e4fa39747b261d2f27b604c5c9
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
