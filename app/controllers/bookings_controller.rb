0c3f68c770ef4fbf7750623513fba7d691c9da79
Niklas Bakula <niklasjbakula@gmail.com>
2023-03-01 15:03:59 +0000

    Added tables to dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,12 +13,33 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.car_id = params[:car_id]
-    @booking.user_id = User.first.id #-> Change this to current user!
+    @booking.user_id = current_user.id
     if @booking.save
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def confirm
+    @booking = Booking.find(params[:id])
+    @booking.status = "confirmed"
+    @booking.save
+    redirect_to dashboard_path, status: :see_other
+  end
+
+  def cancel
+    @booking = Booking.find(params[:id])
+    @booking.status = "cancelled"
+    @booking.save
+    redirect_to dashboard_path, status: :see_other
+  end
+
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.status = "declined"
+    @booking.save
+    redirect_to dashboard_path, status: :see_other
   end
 
   # def edit
